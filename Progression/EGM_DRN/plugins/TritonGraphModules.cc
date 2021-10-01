49d67c0f13f2962edc70cccb9c4dc771b433ab63
Simon Rothman <srothman@mit.edu>
2021-10-01 07:17:01 -0500

    Adding input handling...

--- Progression/EGM_DRN/plugins/TritonGraphModules.cc
+++ Progression/EGM_DRN/plugins/TritonGraphModules.cc
@@ -32,12 +32,55 @@
 
 class TritonGraphHelper {
 public:
-  TritonGraphHelper(edm::ParameterSet const& cfg)
-      : nodeMin_(cfg.getParameter<unsigned>("nodeMin")),
-        nodeMax_(cfg.getParameter<unsigned>("nodeMax")),
-        brief_(cfg.getParameter<bool>("brief")) {}
-  void makeInput(edm::Event const& iEvent, TritonInputMap& iInput, const std::string& debugName) const {
+    TritonGraphHelper(edm::ParameterSet const& cfg)
+        : nodeMin_(cfg.getParameter<unsigned>("nodeMin")),
+          nodeMax_(cfg.getParameter<unsigned>("nodeMax")),
+          brief_(cfg.getParameter<bool>("brief")) {}
+
+    void setEvent(const edm::Event& event) {
+        event.getByToken(tokenEBRecHits_, recHitsEB_);
+        event.getByToken(tokenEERecHits_, recHitsEE_);
+    }
+
+    void setEventSetup(const edm::EventSetup& es) {
+        caloTopo_ = &es.getData(caloTopoToken_);
+        caloGeom_ = &es.getData(caloGeomToken_);
+    }
+
+    void makeInput(reco::SuperCluster& sc, TritonInputMap& iInput, const std::string& debugName) const {
+        const reco::CaloCluster& seedCluster = *(sc.seed());
+        const bool iseb = seedCluster.hitsAndFractions()[0].first.subdetId() == EcalBarrel;
+        const EcalRecHitCollection* recHits = iseb ? recHitsEB_.product() : recHitsEE.product(); 
+
+        int nHits = recHits.size();
+        //
+        //set shapes
+        auto& input1 = iInput.at("x__0");
+        input1.setShape(0, nnodes);
+        auto data1 = input1.allocate<float>();
+        auto& vdata1 = (*data1)[0];
+
+        auto& input2 = iInput.at("batch__1");
+        input2.setShape(0, nnodes);
+        auto data2 = input2.allocate<int64_t>();
+        auto& vdata2 = (*data2)[0];
+
+        auto& input3 = iInput.at("graphx__2");
+        input3.setShape(0, 2);
+        auto data3 = input3.allocate<float>();
+        auto& vdata3 = (*data3)[0];
+
+        for (unsigned i=0;  i<
+   
+    }
+
+
+    void makeInput(edm::Event const& iEvent, TritonInputMap& iInput, const std::string& debugName) const {
+
+
     
+    
+
     //get event-based seed for RNG
     unsigned int runNum_uint = static_cast<unsigned int>(iEvent.id().run());
     unsigned int lumiNum_uint = static_cast<unsigned int>(iEvent.id().luminosityBlock());
@@ -128,6 +171,9 @@ public:
         this->setTokens(cfg);
         batchSize = cfg.getParameter<unsigned>("batchSize");
     }
+    void beginLuminosityBlock(const edm::LuminosityBlock& iLumi, const edm::EventSetup& iSetup){
+        helper_.setEventSetup(iSetup);
+    }
     void acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& iInput) override {
         //client_->setBatchSize(batchSize);
         helper_.makeInput(iEvent, iInput, debugName_);
@@ -142,7 +188,7 @@ public:
             corrSCs->back().setEnergy(1.0);
             corrSCs->back().setCorrectedEnergy(2.0);
             corrSCs->back().setCorrectedEnergyUncertainty(3.0);
-            edm::LogInfo (debugName_) << "supercluster! ";
+//            edm::LogInfo (debugName_) << "supercluster! ";
         }
 
         iEvent.put(std::move(corrSCs));
@@ -157,6 +203,8 @@ public:
         TritonGraphHelper::fillPSetDescription(desc);
         desc.add<edm::InputTag>("inputSCs", edm::InputTag("particleFlowSuperClusterECAL"));
         desc.add<unsigned>("batchSize", 1);
+        desc.add<edm::InputTag>("ecalRecHitsEE", edm::InputTag("ecalRecHit", "EcalRecHitsEE"));
+        desc.add<edm::InputTag>("ecalRecHitsEB", edm::InputTag("ecalRecHit", "EcalRecHitsEB"));
         //to ensure distinct cfi names
         descriptions.addWithDefaultLabel(desc);
     }
@@ -164,6 +212,12 @@ public:
     void setTokens(const edm::ParameterSet& iConfig){
         //inputSCToken_ = cc.consumes<reco::SuperClusterCollection>(iConfig.getParameter<edm::InputTag>("inputSCs"));
         inputSCToken_ = consumes<reco::SuperClusterCollection>(iConfig.getParameter<edm::InputTag>("inputSCs"));
+
+        caloGeomToken_ = cc.esConsumes<CaloGeometry, CaloGeometryRecord, esTransition>();
+        caloTopoToken_ = cc.esConsumes<CaloTopology, CaloTopologyRecord, esTransition>();
+
+        tokenEBRecHits_ = cc.consumes<EcalRecHitCollection>(iConfig.getParameter<edm::InputTag>("ecalRecHitsEB"));
+        tokenEERecHits_ = cc.consumes<EcalRecHitCollection>(iConfig.getParameter<edm::InputTag>("ecalRecHitsEE"));
     }
 
 private:
@@ -173,7 +227,15 @@ private:
     unsigned batchSize;
 
     edm::EDGetTokenT<reco::SuperClusterCollection> inputSCToken_;
-    //const edm::EDGetToken<reco::SuperClusterCollection> inputSCToken_;
+
+    edm::ESGetToken<CaloTopology, CaloTopologyRecord> caloTopoToken_;
+    edm::ESGetToken<CaloGeometry, CaloGeometryRecord> caloGeomToken_;
+
+    edm::EDGetTokenT<EcalRecHitCollection> tokenEBRecHits_;
+    edm::EDGetTokenT<EcalRecHitCollection> tokenEERecHits_;
+
+    edm::Handle<EcalRecHitCollection> recHitsEB_;
+    edm::Handle<EcalRecHitCollection> recHitsEE_;
 };
 
 DEFINE_FWK_MODULE(DRNProducer);
