7ba45943d77fdf7ad89e35ee336ba4bc4e679c78
Simon Rothman <srothman@mit.edu>
2022-01-28 13:42:22 -0600

    Producer template which compiles

--- RecoEgamma/EgammaPhotonProducers/src/PhotonDRNCorrectionProducer.cc
+++ RecoEgamma/EgammaPhotonProducers/src/PhotonDRNCorrectionProducer.cc
@@ -1,21 +1,31 @@
 #include "HeterogeneousCore/SonicTriton/interface/TritonEDProducer.h"
 #include "HeterogeneousCore/SonicTriton/interface/TritonData.h"
 
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "DataFormats/EgammaReco/interface/SuperCluster.h"
-#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
-#include "DataFormats/Common/interface/ValueMap.h"
+#include "FWCore/Utilities/interface/Exception.h"
 
-#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
+#include "DataFormats/EgammaCandidates/interface/PhotonCore.h"
+
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/Common/interface/Handle.h"
 
 #include <sstream>
 #include <string>
 #include <vector>
 #include <random>
+
+#include <iostream>
 
 /*
  * PhotonDRNCorrectionProducer
@@ -42,21 +52,29 @@ class PhotonDRNCorrectionProducer : public TritonEDProducer<> {
 public:
   explicit PhotonDRNCorrectionProducer(const edm::ParameterSet& iConfig);
 
-  void beginLuminosityBlock(const edm::LuminosityBlock& iLumi, const edm::EventSetup& iSetup) override;
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
 
   void acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& input) override;
   void produce(edm::Event& iEvent, const edm::EventSetup& iSetup, Output const& iOutput) override;
 
-  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+  void beginLuminosityBlock(const edm::LuminosityBlock& iLumi, const edm::EventSetup& iSetup) override;
 
 private:
-  edm::EDGetTokenT<reco::SuperClusterCollection> inputSCToken_;
+  const edm::InputTag photonProducer_;
+  edm::EDGetTokenT<edm::View<reco::PhotonCore>> photonCoreToken_;
+  edm::Handle<edm::View<reco::PhotonCore>> photonCoreHandle_;
+
+  std::vector<std::pair<float,float>> corrections_;
+
 };
 
 PhotonDRNCorrectionProducer::PhotonDRNCorrectionProducer(const edm::ParameterSet& iConfig)
     : TritonEDProducer<>(iConfig, "PhotonDRNCorrectionProducer"),
-      inputSCToken_(consumes<reco::SuperClusterCollection>(iConfig.getParameter<edm::InputTag>("inputSCs"))) {
-  produces<reco::SuperClusterCollection>();
+      photonProducer_{iConfig.getParameter<edm::InputTag>("photonProducer")},
+      photonCoreToken_(consumes(photonProducer_)) {
+
+          produces<edm::ValueMap<std::pair<float,float>>>("DRNCorrection");
 }
 
 void PhotonDRNCorrectionProducer::beginLuminosityBlock(const edm::LuminosityBlock& iLumi,
@@ -64,36 +82,50 @@ void PhotonDRNCorrectionProducer::beginLuminosityBlock(const edm::LuminosityBloc
 }
 
 void PhotonDRNCorrectionProducer::acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& iInput) {
-  const auto& inputSCs = iEvent.get(inputSCToken_);
 
-  if (inputSCs.empty()) {
-    client_->setBatchSize(0);
-    return;
-  } else {
-    client_->setBatchSize(1);
-  }
+    photonCoreHandle_ = iEvent.getHandle(photonCoreToken_); 
 
+    if(!photonCoreHandle_.isValid()){
+        throw cms::Exception("PhotonDRNCorrectionProducer") 
+            << "Error! Cant get the photonCoreProducert " 
+            << photonProducer_.label() << std::endl;
+    }
+
+    //clear previous output
+    corrections_.clear();
+    corrections_.reserve(photonCoreHandle_->size());
+
+    if (photonCoreHandle_->empty()){
+        client_->setBatchSize(0);
+        return;
+    } else {
+        client_->setBatchSize(0); //TEMPORARY: don't run anything in Triton yet
+    }
+
+    //setup server input...
 }
 
 void PhotonDRNCorrectionProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup, Output const& iOutput) {
-  const auto& inputSCs = iEvent.get(inputSCToken_);
-  if (inputSCs.empty())
-    return;
+    if (photonCoreHandle_->empty())
+        return;
 
-  auto corrSCs = std::make_unique<reco::SuperClusterCollection>();
-  unsigned i = 0;
-  for (const auto& inputSC : inputSCs) {
+    for (const auto& pho: *photonCoreHandle_){
+        corrections_.emplace_back(std::pair<float,float>(1.0f,2.0f)); //TEMPORARY OUTPUT
+    }
 
-  }
+    //fill
+    auto out = std::make_unique<edm::ValueMap<std::pair<float,float>>>();
+    edm::ValueMap<std::pair<float,float>>::Filler filler(*out);
+    filler.insert(photonCoreHandle_, corrections_.begin(), corrections_.end());
 
-  auto scHandle = iEvent.put(std::move(corrSCs));
+    iEvent.put(std::move(out), "DRNCorrection");
 }
 
 void PhotonDRNCorrectionProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   TritonClient::fillPSetDescription(desc);
-  desc.add<edm::InputTag>("inputSCs", edm::InputTag("particleFlowSuperClusterECAL"));
-  descriptions.add("scEnergyCorrectorDRNProducer", desc);
+  desc.add<edm::InputTag>("photonProducer");
+  descriptions.add("PhotonDRNCorrectionProducer", desc);
 }
 
 DEFINE_FWK_MODULE(PhotonDRNCorrectionProducer);
