b9872bd5cc054eba9cbd3684a94215a41810cf64
Kenneth Long <kdlong@hep.wisc.edu>
2022-02-15 11:20:03 +0100

    Add some PFClusters

--- DPGAnalysis/PFNanoAOD/plugins/SimplePFObjectFlatTableProducerPlugins.cc
+++ DPGAnalysis/PFNanoAOD/plugins/SimplePFObjectFlatTableProducerPlugins.cc
@@ -3,5 +3,8 @@
 #include "SimDataFormats/PFAnalysis/interface/PFTruthParticle.h"
 typedef SimpleFlatTableProducer<PFTruthParticle> SimplePFTruthParticleFlatTableProducer;
 
+#include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
+typedef SimpleFlatTableProducer<reco::PFCluster> SimplePFClusterFlatTableProducer;
+
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(SimplePFTruthParticleFlatTableProducer);
--- DPGAnalysis/PFNanoAOD/python/pfCands_cff.py
+++ DPGAnalysis/PFNanoAOD/python/pfCands_cff.py
@@ -15,18 +15,37 @@ pfCandTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     )
 )
 
-pfTICLCandTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
-    src = cms.InputTag("pfTICL"),
+ecalPFClusTable = cms.EDProducer("SimplePFClusterFlatTableProducer",
+    src = cms.InputTag("particleFlowClusterECAL"),
     cut = cms.string(""), 
-    name = cms.string("PFTICLCand"),
-    doc  = cms.string("ParticleFlow candidates"),
+    name = cms.string("PFClusterECAL"),
+    doc  = cms.string("Particle flow cluters in ECAL"),
     singleton = cms.bool(False), # the number of entries is variable
     extension = cms.bool(False), 
     variables = cms.PSet(CandVars,
-        Vtx_x = Var('vertex().x()', 'float', precision=14, doc='vertex x pos'),
-        Vtx_y = Var('vertex().y()', 'float', precision=14, doc='vertex y pos'),
-        Vtx_z = Var('vertex().z()', 'float', precision=14, doc='vertex z pos'),
+        depth = Var('depth', 'float', precision=14, doc='Cluster depth'),
+        energy = Var('energy', 'float', precision=14, doc='vertex y pos'),
+        correctedEnergy = Var('correctedEnergy', 'float', precision=14, doc='vertex z pos'),
+        algo = Var('algo', 'int', precision=10, doc="Algo ID"),
+        x = Var('x', 'float', precision=14, doc='x position'),
+        y = Var('y', 'float', precision=14, doc='y position'),
+        z = Var('z', 'float', precision=14, doc='z position'),
+        time = Var('time', 'float', precision=14, doc='time'),
     )
 )
 
-pfCandTables = cms.Sequence(pfCandTable+pfTICLCandTable)
+hcalPFClusTable = ecalPFClusTable.clone()
+hcalPFClusTable.src = "particleFlowClusterHCAL"
+hcalPFClusTable.name = "PFClusterHCAL"
+hcalPFClusTable.doc =  "Particle flow cluster in HGCAL"
+
+hfPFClusTable = ecalPFClusTable.clone()
+hfPFClusTable.src = "particleFlowClusterHF"
+hfPFClusTable.name = "PFClusterHF"
+hfPFClusTable.doc =  "Particle flow cluster in HGCAL"
+
+pfCandTables = cms.Sequence(pfCandTable
+        +ecalPFClusTable
+        +hcalPFClusTable
+        +hfPFClusTable
+)
