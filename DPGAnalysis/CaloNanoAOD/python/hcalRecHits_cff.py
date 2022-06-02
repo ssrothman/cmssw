959fb3efb8db03fdb51d5b70911a0b8607c95112
Kenneth Long <kdlong@hep.wisc.edu>
2022-06-02 14:21:09 +0200

    Add HF and HO hits

--- DPGAnalysis/CaloNanoAOD/python/hcalRecHits_cff.py
+++ DPGAnalysis/CaloNanoAOD/python/hcalRecHits_cff.py
@@ -5,7 +5,7 @@ hbheRecHitsTable = cms.EDProducer("SimpleCaloRecHitFlatTableProducer",
     src = cms.InputTag("hbhereco"),
     cut = cms.string(""), 
     name = cms.string("RecHitHBHE"),
-    doc  = cms.string("HCAL RecHits"),
+    doc  = cms.string("HCAL barrel and endcap rec hits"),
     singleton = cms.bool(False), # the number of entries is variable
     extension = cms.bool(False), # this is the main table for the muons
     variables = cms.PSet(
@@ -22,7 +22,33 @@ hbheRecHitPositionTable = cms.EDProducer("HCALRecHitPositionTableProducer",
     doc  = hbheRecHitsTable.doc,
 )
 
+hfRecHitsTable = hbheRecHitsTable.clone()
+hfRecHitsTable.src = "hfreco"
+hfRecHitsTable.name = "RecHitHF"
+hfRecHitsTable.doc = "HCAL forward (HF) rechits"
+
+hfRecHitPositionTable = hbheRecHitPositionTable.clone()
+hfRecHitPositionTable.src = hfRecHitsTable.src
+hfRecHitPositionTable.cut = hfRecHitsTable.cut
+hfRecHitPositionTable.name = hfRecHitsTable.name
+hfRecHitPositionTable.doc  = hfRecHitsTable.doc
+
+hoRecHitsTable = hbheRecHitsTable.clone()
+hoRecHitsTable.src = "horeco"
+hoRecHitsTable.name = "RecHitHO"
+hoRecHitsTable.doc = "HCAL outer (HO) rechits"
+
+hoRecHitPositionTable = hbheRecHitPositionTable.clone()
+hoRecHitPositionTable.src = hoRecHitsTable.src
+hoRecHitPositionTable.cut = hoRecHitsTable.cut
+hoRecHitPositionTable.name = hoRecHitsTable.name
+hoRecHitPositionTable.doc  = hoRecHitsTable.doc
+
 hcalRecHitTables = cms.Sequence(
 	hbheRecHitsTable
 	+hbheRecHitPositionTable 
+    +hfRecHitsTable
+    +hfRecHitPositionTable
+    +hoRecHitsTable
+    +hoRecHitPositionTable
 )
