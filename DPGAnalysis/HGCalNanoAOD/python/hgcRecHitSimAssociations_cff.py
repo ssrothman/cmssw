c33511740b5ccb999a7a32c45f5abe4d80373793
Kenneth Long <kdlong@hep.wisc.edu>
2022-09-28 19:56:42 +0200

    Start trying to make consistent with PFNano

--- DPGAnalysis/HGCalNanoAOD/python/hgcRecHitSimAssociations_cff.py
+++ DPGAnalysis/HGCalNanoAOD/python/hgcRecHitSimAssociations_cff.py
@@ -7,17 +7,7 @@ hgcRecHitsToSimClusters = cms.EDProducer("SimClusterRecHitAssociationProducer",
     simClusters = cms.InputTag("mix:MergedCaloTruth"),
 )
 
-hgcRecHitsToMergedSimClusters = cms.EDProducer("SimClusterRecHitAssociationProducer",
-    caloRecHits = cms.VInputTag("hgcRecHits"),
-    simClusters = cms.InputTag("hgcSimTruth"),
-)
-
-hgcRecHitsToMergedDRSimClusters = cms.EDProducer("SimClusterRecHitAssociationProducer",
-    caloRecHits = cms.VInputTag("hgcRecHits"),
-    simClusters = cms.InputTag("hgcSimTruthDR"),
-)
-
-hgcRecHitsToSimClusterTable = cms.EDProducer("CaloRecHitToSimClusterIndexTableProducer",
+hgcRecHitsToSimClusterTable = cms.EDProducer("HGCRecHitToSimClusterIndexTableProducer",
     cut = hgcRecHitsTable.cut,
     src = hgcRecHitsTable.src,
     objName = hgcRecHitsTable.name,
@@ -25,25 +15,6 @@ hgcRecHitsToSimClusterTable = cms.EDProducer("CaloRecHitToSimClusterIndexTablePr
     objMap = cms.InputTag("hgcRecHitsToSimClusters:hgcRecHitsToSimClus"),
     bestMatchTable = cms.untracked.bool(True),
     docString = cms.string("All SimCluster responsible for sim energy in RecHit DetId (ordered by fraction of energy)")
-)
-
-hgcRecHitsToMergedSimClusterTable = cms.EDProducer("CaloRecHitToSimClusterIndexTableProducer",
-    cut = hgcRecHitsTable.cut,
-    src = hgcRecHitsTable.src,
-    objName = hgcRecHitsTable.name,
-    branchName = cms.string("MergedSimCluster"),
-    objMap = cms.InputTag("hgcRecHitsToMergedSimClusters:hgcRecHitsToSimClus"),
-    bestMatchTable = cms.untracked.bool(True),
-    docString = cms.string("MergedSimClusters ordered by most sim energy in RecHit DetId")
-)
-
-hgcRecHitsToMergedDRSimClusterTable = cms.EDProducer("CaloRecHitToSimClusterIndexTableProducer",
-    cut = hgcRecHitsTable.cut,
-    src = hgcRecHitsTable.src,
-    objName = hgcRecHitsTable.name,
-    branchName = cms.string("MergedByDRSimCluster"),
-    objMap = cms.InputTag("hgcRecHitsToMergedDRSimClusters:hgcRecHitsToSimClus"),
-    docString = cms.string("MergedSimCluster responsible for most sim energy in RecHit DetId")
 )
 
 simClusterRecEnergyTable = cms.EDProducer("SimClusterRecEnergyTableProducer",
@@ -55,21 +26,7 @@ simClusterRecEnergyTable = cms.EDProducer("SimClusterRecEnergyTableProducer",
     docString = cms.string("SimCluster deposited reconstructed energy associated to SimCluster")
 )
 
-mergedSimClusterRecEnergyTable = cms.EDProducer("SimClusterRecEnergyTableProducer",
-    src = cms.InputTag("hgcSimTruth"),
-    cut = cms.string(""),
-    objName = cms.string("MergedSimCluster"),
-    branchName = cms.string("recEnergy"),
-    valueMap = cms.InputTag("hgcRecHitsToMergedSimClusters"),
-    docString = cms.string("SimCluster deposited reconstructed energy associated to SimCluster")
-)
-
-hgcRecHitSimAssociationSequence = cms.Sequence(hgcRecHitsToSimClusters
-                +hgcRecHitsToMergedSimClusters
-                +hgcRecHitsToMergedDRSimClusters
-                +simClusterRecEnergyTable 
-                +mergedSimClusterRecEnergyTable 
-                +hgcRecHitsToSimClusterTable
-                +hgcRecHitsToMergedSimClusterTable
-                +hgcRecHitsToMergedDRSimClusterTable
+hgcRecHitSimAssociationTask = cms.Task(hgcRecHitsToSimClusters,
+	simClusterRecEnergyTable,
+	hgcRecHitsToSimClusterTable
 )
