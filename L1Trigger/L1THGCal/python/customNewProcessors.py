6e150d3d18fdf158a35aa6ae8bbc46b37079dd31
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2022-12-22 12:33:29 +0100

    fix naming in customNewProcessors.py

--- L1Trigger/L1THGCal/python/customNewProcessors.py
+++ L1Trigger/L1THGCal/python/customNewProcessors.py
@@ -21,7 +21,7 @@ def custom_stage1_truncation(process):
 def custom_clustering_standalone(process):
     process.l1tHGCalBackEndLayer2Producer.ProcessorParameters.ProcessorName = cms.string('HGCalBackendLayer2Processor3DClusteringSA')
     process.l1tHGCalBackEndLayer2Producer.ProcessorParameters.DistributionParameters = truncation_params
-    process.hgcalBackEndLayer2Producer.ProcessorParameters.C3d_parameters.histoMax_C3d_clustering_parameters.layer2FwClusteringParameters = layer2ClusteringFw_Params
+    process.l1tHGCalBackEndLayer2Producer.ProcessorParameters.C3d_parameters.histoMax_C3d_clustering_parameters.layer2FwClusteringParameters = layer2ClusteringFw_Params
     return process
 
 def custom_tower_standalone(process):
