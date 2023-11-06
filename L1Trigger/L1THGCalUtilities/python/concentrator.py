15d3059c03ae6ccc2285f4f26b8b5bedd89c308c
Simon Rothman <srothman@mit.edu>
2023-11-06 12:47:42 -0500

    Cleaned up gargage from the TritonAE days

--- L1Trigger/L1THGCalUtilities/python/concentrator.py
+++ L1Trigger/L1THGCalUtilities/python/concentrator.py
@@ -71,90 +71,8 @@ class CreateBestChoice(object):
 from L1Trigger.L1THGCal.ECONTritonProducer_cfi import AEProducer
 from L1Trigger.L1THGCal.MapMergeProducer_cfi import MapMergeProducer
 
-'''
-class CreateTritonAE(object):
-    def __init__(self, bitsPerADC = triton_ae_params.bitsPerADC,
-                bitsPerNorm = triton_ae_params.bitsPerNorm,
-                bitsPerCALQ = triton_ae_params.bitsPerCALQ,
-                bitsPerInput = triton_ae_params.bitsPerInput,
-                bitShiftNormalization = triton_ae_params.bitShiftNormalization,
-                useTransverseADC = triton_ae_params.useTransverseADC,
-                useModuleFactor = triton_ae_params.useModuleFactor,
-                normByMax = triton_ae_params.normByMax,
-                AEProducerName="AEProducer",
-                cuts = [''],
-                modelNames = ['dummy'],
-                verbose=0):
-        #processor params
-        self.processor = triton_ae_params.clone(
-                bitsPerADC = bitsPerADC,
-                bitsPerNorm = bitsPerNorm,
-                bitsPerCALQ = bitsPerCALQ,
-                bitsPerInput = bitsPerInput,
-                bitShiftNormalization = bitShiftNormalization,
-                useTransverseADC = useTransverseADC,
-                useModuleFactor = useModuleFactor,
-                normByMax = normByMax,
-        )
-
-        #AE producer params
-        self.modelNames = modelNames
-        self.cuts = cuts
-        self.AEProducerName = AEProducerName
-        self.verbose = verbose
-        self.bitsPerADC = bitsPerADC
-        self.bitsPerNorm = bitsPerNorm
-        self.bitsPerCALQ = bitsPerCALQ
-        self.bitsPerInput = bitsPerInput
-        self.bitShiftNormalization = bitShiftNormalization
-        self.useTransverseADC = useTransverseADC
-        self.useModuleFactor = useModuleFactor
-        self.normByMax = normByMax
-
-    def __call__(self, process, inputs):
-        mergeinputs = []
-        for i in range(len(self.cuts)):
-            name = self.AEProducerName+"%d"%i
-            mergeinputs.append(name)
-            setattr(process, name, AEProducer.clone(
-                bitsPerADC = self.bitsPerADC,
-                bitsPerNorm = self.bitsPerNorm,
-                bitsPerCALQ = self.bitsPerCALQ,
-                bitsPerInput = self.bitsPerInput,
-                bitShiftNormalization = self.bitShiftNormalization,
-                useTransverseADC = self.useTransverseADC,
-                useModuleFactor = self.useModuleFactor,
-                normByMax = self.normByMax,
-                verbose = self.verbose,
-                cut = self.cuts[i],
-                Client = AEProducer.Client.clone(
-                    modelName = self.modelNames[i]+"_ensemble",
-                    modelConfigPath = "L1Trigger/L1THGCal/data/models/%s/config.pbtxt"%(self.modelNames[i]+"_ensemble")
-                )
-            ))
-
-        setattr(process, self.AEProducerName+"Merge", MapMergeProducer.clone(
-          inputs = mergeinputs
-        ))
-
-        setattr(process, self.AEProducerName+"Task", cms.Task(
-            *[getattr(process, name) for name in mergeinputs],
-            getattr(process, self.AEProducerName+"Merge")
-        ))
-        process.L1THGCalTriggerPrimitives.associate(getattr(process, self.AEProducerName+"Task"))
-        producer = process.l1tHGCalConcentratorProducer.clone(
-            InputTriggerCells = cms.InputTag(inputs),
-            InputTriggerSums = cms.InputTag(inputs),
-            ProcessorParameters = self.processor,
-            InputAE = self.AEProducerName+"Merge",
-        )
-        return producer
-'''
-
 class CreateAutoencoder(object):
     def __init__(self,
-            cellRemap = autoEncoder_conc_proc.cellRemap,
-            cellRemapNoDuplicates = autoEncoder_conc_proc.cellRemapNoDuplicates,
             nBitsPerInput = autoEncoder_conc_proc.nBitsPerInput,
             maxBitsPerOutput = autoEncoder_conc_proc.maxBitsPerOutput,
             bitsPerLink = autoEncoder_conc_proc.bitsPerLink,
@@ -176,11 +94,8 @@ class CreateAutoencoder(object):
             useModuleFactor = autoEncoder_conc_proc.useModuleFactor, 
             bitShiftNormalization = autoEncoder_conc_proc.bitShiftNormalization,
             normByMax = autoEncoder_conc_proc.normByMax,
-            scintillatorMethod = 'thresholdSelect',
-            ):
-         self.processor = autoEncoder_conc_proc.clone(
-                cellRemap = cellRemap,
-                cellRemapNoDuplicates = cellRemapNoDuplicates,
+            scintillatorMethod = 'thresholdSelect'):
+        self.processor = autoEncoder_conc_proc.clone(
                 encoderShape = encoderShape,
                 decoderShape = decoderShape,
                 bitsPerADC = bitsPerADC,
@@ -202,7 +117,7 @@ class CreateAutoencoder(object):
                 threshold_scintillator = threshold_scintillator,
                 threshold_silicon = threshold_silicon,
                 Method = cms.vstring(['autoEncoder','autoEncoder', scintillatorMethod]),
-                )
+        )
 
     def __call__(self, process, inputs):
         producer = process.l1tHGCalConcentratorProducer.clone(
