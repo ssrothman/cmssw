127ec515886911280ae20841f282e26b965bf5b2
Marino Missiroli <m.missiroli@cern.ch>
2025-01-12 17:38:27 +0100

    Revert "Fixes mismatches between emulator and fw on CDC dimuon seed"
    This reverts commit c8aefec7c49eb9b9106d4544be4b73853f863822.

--- L1Trigger/L1TGlobal/plugins/L1TGlobalProducer.cc
+++ L1Trigger/L1TGlobal/plugins/L1TGlobalProducer.cc
@@ -255,6 +255,8 @@ L1TGlobalProducer::L1TGlobalProducer(const edm::ParameterSet& parSet)
   m_l1GtTmAlgoCacheID = 0ULL;
   m_l1GtTmVetoAlgoCacheID = 0ULL;
 
+  m_currentLumi = 0;
+
   // Set default, initial, dummy prescale factor table
   std::vector<std::vector<double>> temp_prescaleTable;
 
